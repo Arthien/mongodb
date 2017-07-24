30838daeedd46760f447302eddff6e029e760943
Arthien <christian.j.green@gmail.com>
2017-07-24 10:55:12 -0400

    Use master branch of db connection

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Mongodb.Mixfile do
 
   defp deps do
     [{:connection,    "~> 1.0"},
-     {:db_connection, "~> 1.1.2"},
+     {:db_connection, git: "https://github.com/elixir-ecto/db_connection", branch: "master"},
      {:poolboy,       ">= 0.0.0", only: :test},
      {:ex_doc,        ">= 0.0.0", only: :dev},
      {:earmark,       ">= 0.0.0", only: :dev}]
